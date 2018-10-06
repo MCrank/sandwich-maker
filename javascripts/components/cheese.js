49d9b9a007de3eb4bdaad1e9ee7bb4172691236f
MCrank <marco_mcse@hotmail.com>
2018-10-06 13:35:48 -0500

    added get, set, string builder functions for cheese

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -1,1 +1,26 @@
-console.log('cheese');
+import { printToDom } from '../helpers/utils.js';
+
+let myCheeses = [];
+
+const setMyCheeses = (newArray) => {
+  myCheeses = newArray;
+};
+
+const getMyCheeses = () => {
+  return myCheeses;
+};
+
+const cheeseListBuilder = (cheeseArray) => {
+  let newString = '';
+  cheeseArray.forEach((cheese) => {
+    newString += `<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="${cheese.name}" id="defaultCheck1">
+    <label class="form-check-label" for="defaultCheck1">
+        ${cheese.name} ($${cheese.price})
+    </label>
+</div>`;
+  });
+  printToDom(newString, 'cheese-content');
+};
+
+export { setMyCheeses, getMyCheeses, cheeseListBuilder };
