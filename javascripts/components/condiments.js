ca7041c954174eb23088c9c91910d432880cc9c3
MCrank <marco_mcse@hotmail.com>
2018-10-06 13:55:49 -0500

    Added get, set, stringbuilder functions to condiments data

--- javascripts/components/condiments.js
+++ javascripts/components/condiments.js
@@ -1,1 +1,26 @@
-console.log('condiments');
+import { printToDom } from '../helpers/utils.js';
+
+let myCondiments = [];
+
+const setMyCondiments = (newArray) => {
+  myCondiments = newArray;
+};
+
+const getMyCondiments = () => {
+  return myCondiments;
+};
+
+const condimentsListBuilder = (condimentsArray) => {
+  let newString = '';
+  condimentsArray.forEach((condiment) => {
+    newString += `<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="${condiment.name}" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+            ${condiment.name} ($${condiment.price})
+        </label>
+    </div>`;
+  });
+  printToDom(newString, 'condiment-content');
+};
+
+export { setMyCondiments, getMyCondiments, condimentsListBuilder };
